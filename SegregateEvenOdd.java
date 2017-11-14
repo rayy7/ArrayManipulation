de33e91dfb3b47727580e4042f780a45c33ea440
rayy7 <33204625+rayy7@users.noreply.github.com>
2017-11-14 19:26:35 -0400

    Update SegregateEvenOdd.java

--- SegregateEvenOdd.java
+++ SegregateEvenOdd.java
@@ -1,13 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Segregate even and odd numbers: time o(n), space O(1)
+ * input: 12,34,5,6,7,23,45,67,89,1
+ * output: 12, 34, 6, 5, 7, 23, 45, 67, 89, 1
  */
 package segregateevenodd;
 
 /**
  *
- * @author md
+ * @author rumana aktar
  */
 public class SegregateEvenOdd {
 
@@ -23,21 +23,18 @@ public class SegregateEvenOdd {
     public static void segregateEvenOdd(int num[]){
         int low=0, high=num.length-1;
         while(low<high){
-            if(num[low]%2==0)
+            if(num[low]%2==0) //if it is even, keep going
                 low++;
             else{
-                while(high>=0 && num[high]%2==1)
+                while(high>=0 && num[high]%2==1) //if it is odd, swap with the first even from end
                     high--;                
                 if(low>=high)
                     break;
                 swap(num, low, high);
                 low++;high--;
-            }
-                
+            }                
         }
-        for(int i=0; i<num.length; i++)
-            System.out.print(num[i]+" ");
-    
+        
     }
     public static void main(String[] args) {
         // TODO code application logic here
