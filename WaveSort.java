5c91309dc7dfa3dec5f715bf1fb85755d8ce57a9
rayy7 <33204625+rayy7@users.noreply.github.com>
2017-11-14 13:40:28 -0400

    Update WaveSort.java

--- WaveSort.java
+++ WaveSort.java
@@ -1,4 +1,6 @@
-/*
+/* for every odd index, num[odd-1]<num[odd] && num[odd+1]<num[odd]
+ * input:  10,  12,  2,  3,  4,  5,  1,  11, 23
+ * output: 10,  12,  2,  4,  3,  5,  1,  23,  11 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,35 +17,27 @@ public class WaveSort {
      * @param args the command line arguments
      */
     
+    //swap elements
     public static void swap(int num[], int x, int y){
         int temp=num[x];
         num[x]=num[y];
         num[y]=temp;
     }
-    public static void waveSort(int num[]){
-        
+    //for every odd index, num[odd-1]<num[odd] && num[odd+1]<num[odd]
+    public static void waveSort(int num[]){        
         for(int largeIndex=1; largeIndex<num.length; ){
-            System.out.println(largeIndex);
-            if(num[largeIndex]<num[largeIndex-1])
+            if(num[largeIndex]<num[largeIndex-1]) //check previous element
                 swap(num, largeIndex-1, largeIndex);
-            if((largeIndex+1 <num.length) && num[largeIndex]<num[largeIndex+1])
+            if((largeIndex+1 <num.length) && num[largeIndex]<num[largeIndex+1]) //check next elemet
                 swap(num, largeIndex, largeIndex+1);
-            largeIndex=largeIndex+2;
-            
+            largeIndex=largeIndex+2;            
         }
-        
-        printArray(num);
-            
     }
-     public static void printArray(int num[]){
-        System.out.print("\n\nThe array is: ");
-        for(int i=0; i<num.length; i++)
-            System.out.print(num[i]+"  ");
-    }
+     
     public static void main(String[] args) {
         // TODO code application logic here
         int num[]={10,12,2,3,4,5,1,11,23};
-        waveSort(num);
+        waveSort(num); // 10  12  2  4  3  5  1  23  11 
     }
     
 }
