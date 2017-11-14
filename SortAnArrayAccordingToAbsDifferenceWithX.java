0ed0b0259d9a0da1a0f8ef5b072def16227a079f
rayy7 <33204625+rayy7@users.noreply.github.com>
2017-11-14 15:35:57 -0400

    Update SortAnArrayAccordingToAbsDifferenceWithX.java

--- SortAnArrayAccordingToAbsDifferenceWithX.java
+++ SortAnArrayAccordingToAbsDifferenceWithX.java
@@ -1,13 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
  */
 package sortanarrayaccordingtoabsdifferencewithx;
 
 /**
  *
- * @author md
+ * @author rumana aktar
  */
 
 class Number{
@@ -24,14 +21,11 @@ public class SortAnArrayAccordingToAbsDifferenceWithX {
     public static Number [] callMergeSort(Number nums[]){//2:06
         return mergeSort(nums, 0, nums.length-1);
     }
-    public static Number [] mergeSort(Number num[], int low, int high){
-        
-        
+    
+    public static Number [] mergeSort(Number num[], int low, int high){        
         if(low<high){
             int mid=(low+high)/2;
-            //System.out.println(low+" "+mid);
-            num=mergeSort(num, low, mid); 
-            //System.out.println(mid+1+" "+high);            
+            num=mergeSort(num, low, mid);             
             num=mergeSort(num, mid+1, high);
             num=merge(num, low, mid, high);
         }
@@ -40,19 +34,15 @@ public class SortAnArrayAccordingToAbsDifferenceWithX {
     
     public static Number [] merge(Number num[], int low, int mid, int high){
         //System.out.println("low="+low+" mid="+mid+" high="+high);
-        Number lowArray[]=new Number[mid-low+1], highArray[]=new Number[high-mid]; int low2=low;
+        Number lowArray[]=new Number[mid-low+1], highArray[]=new Number[high-mid];
+        int low2=low;
+        
         for(int i=0; i<lowArray.length; i++){
             lowArray[i]=num[low++];
-            //System.out.print(lowArray[i]+" ");
-        }
-        //System.out.println();
-        
-        
+        }                
         for(int i=0; i<highArray.length; i++){
             highArray[i]=num[++mid];
-            //System.out.print(highArray[i]+" ");
-        }//System.out.println();  
-        
+        }
         
         int i=0, j=0, k=low2;
         while(i<lowArray.length && j<highArray.length){
@@ -69,25 +59,27 @@ public class SortAnArrayAccordingToAbsDifferenceWithX {
         while(j<highArray.length){
             num[k++]=highArray[j++];
         }
-        
-        
-        
-        
             
         return num;
     }
     
     public static void sortAbsDiff(int num[], int x){
         Number nums[]=new Number[num.length];
+        
+        //generate a data structure with val and absDiff as abs(val-x)
         for(int i=0; i<nums.length; i++){
             nums[i]=new Number(num[i], x);
         }    
         
+        //apply mergersort on the data structure; its nlog(n)
         callMergeSort(nums);
+        
+        //print absolute difference
         for(int k=0; k<nums.length; k++){
             System.out.print(nums[k].absDiff+" ");
         }System.out.println();  
         
+        //print originak difference
         for(int k=0; k<nums.length; k++){
             System.out.print(nums[k].val+" ");
         }System.out.println();  
