e016ca277fc31d4b2889338d37007ac8fe35e014
rayy7 <33204625+rayy7@users.noreply.github.com>
2017-11-14 17:16:32 -0400

    Update and rename Main.java to ThreeWayPartition.java

--- ThreeWayPartition.java
+++ ThreeWayPartition.java
@@ -1,15 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Three way partitioning of an array around a given range
+    Given an array and a range [lowVal, highVal], partition the array around the range such that array is divided in three parts.
+        1) All elements smaller than lowVal come first.
+        2) All elements in range lowVal to highVVal come next.
+        3) All elements greater than highVVal appear in the end.
+    The individual elements of three sets can appear in any order.
+    
+    input: num[]={21,12,3,4,5,9,20,19,5,6,11,32,5}, val1=11, val2=20
+    output: []={5, 3, 4, 5, 9, 5, 6, 19, 12, 20, 11, 32, 21 }
  */
 package pkg3waypartitioning;
 
 /**
  *
- * @author md
+ * @author rumana aktar, idea: geeksforgeeks
  */
-public class Main {
+public class ThreeWayPartition {
 
     /**
      * @param args the command line arguments
@@ -18,12 +24,6 @@ public class Main {
         int temp=num[x];
         num[x]=num[y];
         num[y]=temp;
-    }
-    public static void print(int num[]){
-        for(int i=0; i<num.length; i++)
-            System.out.print(num[i]+" ");
-        System.out.println();
-        
     }
     public static void threeWaySort(int num[], int val1, int val2){
         int low=0, high=num.length-1, mid=0;
@@ -41,9 +41,8 @@ public class Main {
                 mid++;
             }            
         }
-        System.out.println("low index = "+(low-1)+" high index = "+high);
-        print(num);
-                
+        //num[0...(low-1)]<val1, val1>=num[low...high-1]<=val2, num[high...num.length-1]>high
+        System.out.println("low index = "+(low-1)+" high index = "+high);        
     }
     public static void main(String[] args) {
         // TODO code application logic here
